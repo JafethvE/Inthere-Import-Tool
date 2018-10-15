81e56ee62201919c4848d4edf08b0526afffbad9
Jafeth <Jafeth.vanElten@hotmail.com>
2018-10-15 15:40:17 +0200

    Create FileReader class.

---
+++ Assets/Scripts/FileSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class FileSystem {
+
+    public string[] GetFileNamesInFolder(string path)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        FileInfo[] files = directoryInfo.GetFiles();
+        string[] fileNames = new string[files.Length];
+        int counter = 0;
+        foreach (FileInfo file in files)
+        {
+            fileNames[counter] = file.Name;
+            counter++;
+        }
+        return fileNames;
+    }
+
+    public Sprite GetSprite(string path)
+    {
+        return Resources.Load<Sprite>(path);
+    }
+
+    public string GetJSON(string path)
+    {
+        return File.ReadAllText(path);
+    }
+}
